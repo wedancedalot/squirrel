357820d73f4127116f7f92148ae9bfff62eee568
Stanislav Cherviakov <stchervyakov@gmail.com>
2018-07-03 13:21:45 +0300

    union

--- select.go
+++ select.go
@@ -1,147 +1,165 @@
 package squirrel
 
 import (
-    "bytes"
-    "database/sql"
-    "fmt"
-    "strings"
+	"bytes"
+	"database/sql"
+	"fmt"
+	"strings"
 
-    "github.com/lann/builder"
+	"github.com/lann/builder"
 )
 
 type selectData struct {
-    PlaceholderFormat PlaceholderFormat
-    RunWith           BaseRunner
-    Prefixes          exprs
-    Options           []string
-    Columns           []Sqlizer
-    From              Sqlizer
-    Joins             []Sqlizer
-    WhereParts        []Sqlizer
-    GroupBys          []string
-    HavingParts       []Sqlizer
-    OrderBys          []string
-    Limit             string
-    Offset            string
-    Suffixes          exprs
-    ForUpdate         bool
+	PlaceholderFormat PlaceholderFormat
+	RunWith           BaseRunner
+	Prefixes          exprs
+	Options           []string
+	Columns           []Sqlizer
+	From              Sqlizer
+	Joins             []Sqlizer
+	WhereParts        []Sqlizer
+	GroupBys          []string
+	HavingParts       []Sqlizer
+	OrderBys          []string
+	Limit             string
+	Offset            string
+	Union             []Sqlizer
+	UnionAll          []Sqlizer
+	Suffixes          exprs
+	ForUpdate         bool
 }
 
 func (d *selectData) Exec() (sql.Result, error) {
-    if d.RunWith == nil {
-        return nil, RunnerNotSet
-    }
-    return ExecWith(d.RunWith, d)
+	if d.RunWith == nil {
+		return nil, RunnerNotSet
+	}
+	return ExecWith(d.RunWith, d)
 }
 
 func (d *selectData) Query() (*sql.Rows, error) {
-    if d.RunWith == nil {
-        return nil, RunnerNotSet
-    }
-    return QueryWith(d.RunWith, d)
+	if d.RunWith == nil {
+		return nil, RunnerNotSet
+	}
+	return QueryWith(d.RunWith, d)
 }
 
 func (d *selectData) QueryRow() RowScanner {
-    if d.RunWith == nil {
-        return &Row{err: RunnerNotSet}
-    }
-    queryRower, ok := d.RunWith.(QueryRower)
-    if !ok {
-        return &Row{err: RunnerNotQueryRunner}
-    }
-    return QueryRowWith(queryRower, d)
+	if d.RunWith == nil {
+		return &Row{err: RunnerNotSet}
+	}
+	queryRower, ok := d.RunWith.(QueryRower)
+	if !ok {
+		return &Row{err: RunnerNotQueryRunner}
+	}
+	return QueryRowWith(queryRower, d)
 }
 
 func (d *selectData) ToSql() (sqlStr string, args []interface{}, err error) {
-    if len(d.Columns) == 0 {
-        err = fmt.Errorf("select statements must have at least one result column")
-        return
-    }
+	if len(d.Columns) == 0 {
+		err = fmt.Errorf("select statements must have at least one result column")
+		return
+	}
 
-    sql := &bytes.Buffer{}
+	sql := &bytes.Buffer{}
 
-    if len(d.Prefixes) > 0 {
-        args, _ = d.Prefixes.AppendToSql(sql, " ", args)
-        sql.WriteString(" ")
-    }
+	if len(d.Prefixes) > 0 {
+		args, _ = d.Prefixes.AppendToSql(sql, " ", args)
+		sql.WriteString(" ")
+	}
 
-    sql.WriteString("SELECT ")
+	sql.WriteString("SELECT ")
 
-    if len(d.Options) > 0 {
-        sql.WriteString(strings.Join(d.Options, " "))
-        sql.WriteString(" ")
-    }
+	if len(d.Options) > 0 {
+		sql.WriteString(strings.Join(d.Options, " "))
+		sql.WriteString(" ")
+	}
 
-    if len(d.Columns) > 0 {
-        args, err = appendToSql(d.Columns, sql, ", ", args)
-        if err != nil {
-            return
-        }
-    }
+	if len(d.Columns) > 0 {
+		args, err = appendToSql(d.Columns, sql, ", ", args)
+		if err != nil {
+			return
+		}
+	}
 
-    if d.From != nil {
-        sql.WriteString(" FROM ")
-        args, err = appendToSql([]Sqlizer{d.From}, sql, "", args)
-        if err != nil {
-            return
-        }
-    }
+	if d.From != nil {
+		sql.WriteString(" FROM ")
+		args, err = appendToSql([]Sqlizer{d.From}, sql, "", args)
+		if err != nil {
+			return
+		}
+	}
 
-    if len(d.Joins) > 0 {
-        sql.WriteString(" ")
-        args, err = appendToSql(d.Joins, sql, " ", args)
-        if err != nil {
-            return
-        }
-    }
+	if len(d.Joins) > 0 {
+		sql.WriteString(" ")
+		args, err = appendToSql(d.Joins, sql, " ", args)
+		if err != nil {
+			return
+		}
+	}
 
-    if len(d.WhereParts) > 0 {
-        sql.WriteString(" WHERE ")
-        args, err = appendToSql(d.WhereParts, sql, " AND ", args)
-        if err != nil {
-            return
-        }
-    }
+	if len(d.WhereParts) > 0 {
+		sql.WriteString(" WHERE ")
+		args, err = appendToSql(d.WhereParts, sql, " AND ", args)
+		if err != nil {
+			return
+		}
+	}
 
-    if len(d.GroupBys) > 0 {
-        sql.WriteString(" GROUP BY ")
-        sql.WriteString(strings.Join(d.GroupBys, ", "))
-    }
+	if len(d.GroupBys) > 0 {
+		sql.WriteString(" GROUP BY ")
+		sql.WriteString(strings.Join(d.GroupBys, ", "))
+	}
 
-    if len(d.HavingParts) > 0 {
-        sql.WriteString(" HAVING ")
-        args, err = appendToSql(d.HavingParts, sql, " AND ", args)
-        if err != nil {
-            return
-        }
-    }
+	if len(d.HavingParts) > 0 {
+		sql.WriteString(" HAVING ")
+		args, err = appendToSql(d.HavingParts, sql, " AND ", args)
+		if err != nil {
+			return
+		}
+	}
 
-    if len(d.OrderBys) > 0 {
-        sql.WriteString(" ORDER BY ")
-        sql.WriteString(strings.Join(d.OrderBys, ", "))
-    }
+	if len(d.OrderBys) > 0 {
+		sql.WriteString(" ORDER BY ")
+		sql.WriteString(strings.Join(d.OrderBys, ", "))
+	}
 
-    if len(d.Limit) > 0 {
-        sql.WriteString(" LIMIT ")
-        sql.WriteString(d.Limit)
-    }
+	if len(d.Limit) > 0 {
+		sql.WriteString(" LIMIT ")
+		sql.WriteString(d.Limit)
+	}
 
-    if len(d.Offset) > 0 {
-        sql.WriteString(" OFFSET ")
-        sql.WriteString(d.Offset)
-    }
+	if len(d.Offset) > 0 {
+		sql.WriteString(" OFFSET ")
+		sql.WriteString(d.Offset)
+	}
 
-    if len(d.Suffixes) > 0 {
-        sql.WriteString(" ")
-        args, _ = d.Suffixes.AppendToSql(sql, " ", args)
-    }
+	if len(d.Suffixes) > 0 {
+		sql.WriteString(" ")
+		args, _ = d.Suffixes.AppendToSql(sql, " ", args)
+	}
 
-    if d.ForUpdate {
-        sql.WriteString(" FOR UPDATE ")
-    }
+	if len(d.Union) > 0 {
+		sql.WriteString(" UNION ")
+		args, err = appendToSql(d.Union, sql, " UNION ", args)
+		if err != nil {
+			return
+		}
+	}
 
-    sqlStr, err = d.PlaceholderFormat.ReplacePlaceholders(sql.String())
-    return
+	if len(d.UnionAll) > 0 {
+		sql.WriteString(" UNION ALL ")
+		args, err = appendToSql(d.UnionAll, sql, " UNION ALL ", args)
+		if err != nil {
+			return
+		}
+	}
+
+	if d.ForUpdate {
+		sql.WriteString(" FOR UPDATE ")
+	}
+
+	sqlStr, err = d.PlaceholderFormat.ReplacePlaceholders(sql.String())
+	return
 }
 
 // Builder
@@ -150,7 +168,7 @@ func (d *selectData) ToSql() (sqlStr string, args []interface{}, err error) {
 type SelectBuilder builder.Builder
 
 func init() {
-    builder.Register(SelectBuilder{}, selectData{})
+	builder.Register(SelectBuilder{}, selectData{})
 }
 
 // Format methods
@@ -158,69 +176,69 @@ func init() {
 // PlaceholderFormat sets PlaceholderFormat (e.g. Question or Dollar) for the
 // query.
 func (b SelectBuilder) PlaceholderFormat(f PlaceholderFormat) SelectBuilder {
-    return builder.Set(b, "PlaceholderFormat", f).(SelectBuilder)
+	return builder.Set(b, "PlaceholderFormat", f).(SelectBuilder)
 }
 
 // Runner methods
 
 // RunWith sets a Runner (like database/sql.DB) to be used with e.g. Exec.
 func (b SelectBuilder) RunWith(runner BaseRunner) SelectBuilder {
-    return setRunWith(b, runner).(SelectBuilder)
+	return setRunWith(b, runner).(SelectBuilder)
 }
 
 // Exec builds and Execs the query with the Runner set by RunWith.
 func (b SelectBuilder) Exec() (sql.Result, error) {
-    data := builder.GetStruct(b).(selectData)
-    return data.Exec()
+	data := builder.GetStruct(b).(selectData)
+	return data.Exec()
 }
 
 // Query builds and Querys the query with the Runner set by RunWith.
 func (b SelectBuilder) Query() (*sql.Rows, error) {
-    data := builder.GetStruct(b).(selectData)
-    return data.Query()
+	data := builder.GetStruct(b).(selectData)
+	return data.Query()
 }
 
 // QueryRow builds and QueryRows the query with the Runner set by RunWith.
 func (b SelectBuilder) QueryRow() RowScanner {
-    data := builder.GetStruct(b).(selectData)
-    return data.QueryRow()
+	data := builder.GetStruct(b).(selectData)
+	return data.QueryRow()
 }
 
 // Scan is a shortcut for QueryRow().Scan.
 func (b SelectBuilder) Scan(dest ...interface{}) error {
-    return b.QueryRow().Scan(dest...)
+	return b.QueryRow().Scan(dest...)
 }
 
 // SQL methods
 
 // ToSql builds the query into a SQL string and bound args.
 func (b SelectBuilder) ToSql() (string, []interface{}, error) {
-    data := builder.GetStruct(b).(selectData)
-    return data.ToSql()
+	data := builder.GetStruct(b).(selectData)
+	return data.ToSql()
 }
 
 // Prefix adds an expression to the beginning of the query
 func (b SelectBuilder) Prefix(sql string, args ...interface{}) SelectBuilder {
-    return builder.Append(b, "Prefixes", Expr(sql, args...)).(SelectBuilder)
+	return builder.Append(b, "Prefixes", Expr(sql, args...)).(SelectBuilder)
 }
 
 // Distinct adds a DISTINCT clause to the query.
 func (b SelectBuilder) Distinct() SelectBuilder {
-    return b.Options("DISTINCT")
+	return b.Options("DISTINCT")
 }
 
 // Options adds select option to the query
 func (b SelectBuilder) Options(options ...string) SelectBuilder {
-    return builder.Extend(b, "Options", options).(SelectBuilder)
+	return builder.Extend(b, "Options", options).(SelectBuilder)
 }
 
 // Columns adds result columns to the query.
 func (b SelectBuilder) Columns(columns ...string) SelectBuilder {
-    var parts []interface{}
-    for _, str := range columns {
-        parts = append(parts, newPart(str))
-    }
-    return builder.Extend(b, "Columns", parts).(SelectBuilder)
+	var parts []interface{}
+	for _, str := range columns {
+		parts = append(parts, newPart(str))
+	}
+	return builder.Extend(b, "Columns", parts).(SelectBuilder)
 }
 
 // Column adds a result column to the query.
@@ -228,54 +246,53 @@ func (b SelectBuilder) Columns(columns ...string) SelectBuilder {
 // the columns string, for example:
 //   Column("IF(col IN ("+squirrel.Placeholders(3)+"), 1, 0) as col", 1, 2, 3)
 func (b SelectBuilder) Column(column interface{}, args ...interface{}) SelectBuilder {
-    return builder.Append(b, "Columns", newPart(column, args...)).(SelectBuilder)
+	return builder.Append(b, "Columns", newPart(column, args...)).(SelectBuilder)
 }
 
 // From sets the FROM clause of the query.
 func (b SelectBuilder) From(from string) SelectBuilder {
-    return builder.Set(b, "From", newPart(from)).(SelectBuilder)
+	return builder.Set(b, "From", newPart(from)).(SelectBuilder)
 }
 
 // FromSelect sets a subquery into the FROM clause of the query.
 func (b SelectBuilder) FromSelect(from SelectBuilder, alias string) SelectBuilder {
-    return builder.Set(b, "From", Alias(from, alias)).(SelectBuilder)
+	return builder.Set(b, "From", Alias(from, alias)).(SelectBuilder)
 }
 
 // JoinClause adds a join clause to the query.
 func (b SelectBuilder) JoinClause(pred interface{}, args ...interface{}) SelectBuilder {
-    return builder.Append(b, "Joins", newPart(pred, args...)).(SelectBuilder)
+	return builder.Append(b, "Joins", newPart(pred, args...)).(SelectBuilder)
 }
 
 // Join adds a JOIN clause to the query.
 func (b SelectBuilder) Join(join string, rest ...interface{}) SelectBuilder {
-    return b.JoinClause("JOIN " + join, rest...)
+	return b.JoinClause("JOIN "+join, rest...)
 }
 
 // Join adds a JOIN clause to the query.
 func (b SelectBuilder) JoinTable(tableName, fieldName string, sourceTable ...string) SelectBuilder {
-    return b.JoinClause(joinTable("JOIN", tableName, fieldName, sourceTable...))
+	return b.JoinClause(joinTable("JOIN", tableName, fieldName, sourceTable...))
 }
 
 // LeftJoin adds a LEFT JOIN clause to the query.
 func (b SelectBuilder) LeftJoin(join string, rest ...interface{}) SelectBuilder {
-    return b.JoinClause("LEFT JOIN " + join, rest...)
+	return b.JoinClause("LEFT JOIN "+join, rest...)
 }
 
 // Join adds a JOIN clause to the query.
 func (b SelectBuilder) LeftJoinTable(tableName, fieldName string, sourceTable ...string) SelectBuilder {
-    return b.JoinClause(joinTable("LEFT JOIN", tableName, fieldName, sourceTable...))
+	return b.JoinClause(joinTable("LEFT JOIN", tableName, fieldName, sourceTable...))
 }
 
 // RightJoin adds a RIGHT JOIN clause to the query.
 func (b SelectBuilder) RightJoin(join string, rest ...interface{}) SelectBuilder {
-    return b.JoinClause("RIGHT JOIN " + join, rest...)
+	return b.JoinClause("RIGHT JOIN "+join, rest...)
 }
 
 // Join adds a JOIN clause to the query.
 func (b SelectBuilder) RightJoinTable(tableName, fieldName string, sourceTable ...string) SelectBuilder {
-    return b.JoinClause(joinTable("RIGHT JOIN", tableName, fieldName, sourceTable...))
+	return b.JoinClause(joinTable("RIGHT JOIN", tableName, fieldName, sourceTable...))
 }
-
 
 // Where adds an expression to the WHERE clause of the query.
 //
@@ -298,42 +315,52 @@ func (b SelectBuilder) RightJoinTable(tableName, fieldName string, sourceTable .
 //
 // Where will panic if pred isn't any of the above types.
 func (b SelectBuilder) Where(pred interface{}, args ...interface{}) SelectBuilder {
-    return builder.Append(b, "WhereParts", newWherePart(pred, args...)).(SelectBuilder)
+	return builder.Append(b, "WhereParts", newWherePart(pred, args...)).(SelectBuilder)
 }
 
 // GroupBy adds GROUP BY expressions to the query.
 func (b SelectBuilder) GroupBy(groupBys ...string) SelectBuilder {
-    return builder.Extend(b, "GroupBys", groupBys).(SelectBuilder)
+	return builder.Extend(b, "GroupBys", groupBys).(SelectBuilder)
 }
 
 // Having adds an expression to the HAVING clause of the query.
 //
 // See Where.
 func (b SelectBuilder) Having(pred interface{}, rest ...interface{}) SelectBuilder {
-    return builder.Append(b, "HavingParts", newWherePart(pred, rest...)).(SelectBuilder)
+	return builder.Append(b, "HavingParts", newWherePart(pred, rest...)).(SelectBuilder)
 }
 
 // OrderBy adds ORDER BY expressions to the query.
 func (b SelectBuilder) OrderBy(orderBys ...string) SelectBuilder {
-    return builder.Extend(b, "OrderBys", orderBys).(SelectBuilder)
+	return builder.Extend(b, "OrderBys", orderBys).(SelectBuilder)
 }
 
 // Limit sets a LIMIT clause on the query.
 func (b SelectBuilder) Limit(limit uint64) SelectBuilder {
-    return builder.Set(b, "Limit", fmt.Sprintf("%d", limit)).(SelectBuilder)
+	return builder.Set(b, "Limit", fmt.Sprintf("%d", limit)).(SelectBuilder)
 }
 
 // Offset sets a OFFSET clause on the query.
 func (b SelectBuilder) Offset(offset uint64) SelectBuilder {
-    return builder.Set(b, "Offset", fmt.Sprintf("%d", offset)).(SelectBuilder)
+	return builder.Set(b, "Offset", fmt.Sprintf("%d", offset)).(SelectBuilder)
 }
 
 // Suffix adds an expression to the end of the query
 func (b SelectBuilder) Suffix(sql string, args ...interface{}) SelectBuilder {
-    return builder.Append(b, "Suffixes", Expr(sql, args...)).(SelectBuilder)
+	return builder.Append(b, "Suffixes", Expr(sql, args...)).(SelectBuilder)
 }
 
 // Adds for update lock
 func (b SelectBuilder) ForUpdate(enable bool) SelectBuilder {
-    return builder.Set(b, "ForUpdate", enable).(SelectBuilder)
+	return builder.Set(b, "ForUpdate", enable).(SelectBuilder)
+}
+
+// Union adds a UNION clause to the query
+func (b SelectBuilder) Union(query interface{}, args ...interface{}) SelectBuilder {
+	return builder.Append(b, "Union", newUnionPart(query, args...)).(SelectBuilder)
+}
+
+// UnionAll adds a UNION ALL clause to the query
+func (b SelectBuilder) UnionAll(query interface{}, args ...interface{}) SelectBuilder {
+	return builder.Append(b, "UnionAll", newUnionPart(query, args...)).(SelectBuilder)
 }
